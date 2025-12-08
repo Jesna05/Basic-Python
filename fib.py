59518a86a462fc37522b125606abdf24bda1e98c
Jesna05 <jesna056@gmail.com>
2025-12-08 14:18:44 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,3 +1,4 @@
+#Method 1
 def fib(n):
     if n <= 1:
         return n
@@ -6,3 +7,16 @@ def fib(n):
 x = int(input("Enter a number: "))
 for i in range(x):
         print(fib(i))
+
+
+#Method 2
+def fib(n,memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return 1
+    else:
+        memo[n] = fib(n-1,memo) + fib(n-2,memo)
+        return memo[n]
+n = int(input("Enter the value of n:"))
+print(f"The {n}th fibonacci number is: {fib(n)}")
